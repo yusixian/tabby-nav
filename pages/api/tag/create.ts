56fc3f4ad392aae3011cdda8c0edc9edca3a78bb
yusixian <3450414733@qq.com>
2023-07-17 16:33:24 +0800

    fix: prisma categories

--- pages/api/tag/create.ts
+++ pages/api/tag/create.ts
@@ -16,7 +16,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
           id,
         })),
       },
-      category: {
+      categories: {
         connect: categoryIds?.map((id) => ({
           id,
         })),
